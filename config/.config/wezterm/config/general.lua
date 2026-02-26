e8cc410449b84d01a93fc582f7d508927850af00
yoshihiko <yoshihiko05410@gmail.com>
2026-02-26 09:24:13 +0900

    fix: 非アクティブペインの調整

--- config/.config/wezterm/config/general.lua
+++ config/.config/wezterm/config/general.lua
@@ -41,8 +41,8 @@ return {
 
   -- 非アクティブペインを暗くしてアクティブペインを際立たせる
   inactive_pane_hsb = {
-    saturation = 0.5,
-    brightness = 0.3,
+    saturation = 0.8,
+    brightness = 0.6,
   },
 
   -- スクロールバック行数
